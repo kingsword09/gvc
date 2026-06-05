b6e61692e78c7ac97fd7549cbd4e54eef01b9aea
Kingsword <kingsword09@gmail.com>
2026-06-05 19:11:52 +0800

    chore(release): v0.2.1 (#30)

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -33,6 +33,162 @@ info() {
   printf '==> %s\n' "$*"
 }
 
+sentence_case() {
+  awk '{ print toupper(substr($0, 1, 1)) substr($0, 2) }' <<<"$1"
+}
+
+append_commit_entry() {
+  local subject="$1"
+  local added_file="$2"
+  local changed_file="$3"
+  local fixed_file="$4"
+  local docs_file="$5"
+  local kind="changed"
+  local description="$subject"
+  local conventional_regex='^([A-Za-z]+)(\([^)]+\))?(!)?:[[:space:]]*(.+)$'
+
+  case "$subject" in
+    "chore(release): prepare v$version"* | "chore(release): v$version"*)
+      return
+      ;;
+  esac
+
+  if [[ "$subject" =~ $conventional_regex ]]; then
+    kind="${BASH_REMATCH[1]}"
+    description="${BASH_REMATCH[4]}"
+  fi
+
+  description=$(sentence_case "$description")
+
+  case "$kind" in
+    feat)
+      printf -- '- %s\n' "$description" >>"$added_file"
+      ;;
+    fix)
+      printf -- '- %s\n' "$description" >>"$fixed_file"
+      ;;
+    docs)
+      printf -- '- %s\n' "$description" >>"$docs_file"
+      ;;
+    *)
+      printf -- '- %s\n' "$description" >>"$changed_file"
+      ;;
+  esac
+}
+
+print_changelog_section() {
+  local title="$1"
+  local entries_file="$2"
+
+  [[ -s "$entries_file" ]] || return 0
+
+  if [[ ${printed_changelog_section:-0} -eq 1 ]]; then
+    printf '\n'
+  fi
+
+  printf '### %s\n' "$title"
+  cat "$entries_file"
+  printed_changelog_section=1
+}
+
+generate_changelog_body() {
+  local added_file
+  local changed_file
+  local fixed_file
+  local docs_file
+  local previous_tag=""
+  local range=""
+  local subject=""
+  local has_git_commits=0
+
+  added_file=$(mktemp)
+  changed_file=$(mktemp)
+  fixed_file=$(mktemp)
+  docs_file=$(mktemp)
+
+  if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    previous_tag=$(git tag --merged HEAD --list 'v[0-9]*' --sort=-v:refname | awk -v target="v$version" '$0 != target { print; exit }')
+    if [[ -n "$previous_tag" ]]; then
+      range="$previous_tag..HEAD"
+    else
+      range="HEAD"
+    fi
+
+    while IFS= read -r subject || [[ -n "$subject" ]]; do
+      [[ -n "$subject" ]] || continue
+      append_commit_entry "$subject" "$added_file" "$changed_file" "$fixed_file" "$docs_file"
+    done < <(git log --no-merges --reverse --pretty=format:%s "$range")
+  fi
+
+  if [[ -s "$added_file" || -s "$changed_file" || -s "$fixed_file" || -s "$docs_file" ]]; then
+    has_git_commits=1
+  fi
+
+  if [[ $has_git_commits -eq 1 ]]; then
+    printed_changelog_section=0
+    print_changelog_section "Added" "$added_file"
+    print_changelog_section "Changed" "$changed_file"
+    print_changelog_section "Fixed" "$fixed_file"
+    print_changelog_section "Documentation" "$docs_file"
+  else
+    printf '### Changed\n'
+    printf -- '- TODO: Summarize release changes.\n'
+  fi
+
+  rm -f "$added_file" "$changed_file" "$fixed_file" "$docs_file"
+}
+
+replace_changelog_body() {
+  local body_file="$1"
+  local tmp
+  tmp=$(mktemp)
+
+  awk -v version="$version" -v body_file="$body_file" '
+    function emit_body(  line) {
+      while ((getline line < body_file) > 0) {
+        print line
+      }
+      close(body_file)
+    }
+
+    /^## \[/ {
+      if (in_target) {
+        emit_body()
+        print ""
+        in_target = 0
+      }
+
+      if (index($0, "## [" version "]") == 1) {
+        print
+        print ""
+        in_target = 1
+        next
+      }
+    }
+
+    in_target { next }
+    { print }
+
+    END {
+      if (in_target) {
+        emit_body()
+      }
+    }
+  ' CHANGELOG.md >"$tmp"
+  mv "$tmp" CHANGELOG.md
+}
+
+changelog_section_has_todo() {
+  awk -v version="$version" '
+    /^## \[/ {
+      in_target = (index($0, "## [" version "]") == 1)
+      next
+    }
+    in_target && /TODO: Summarize release changes\./ { found = 1 }
+    END { exit found ? 0 : 1 }
+  ' CHANGELOG.md
+}
+
 version=""
 release_date="${RELEASE_DATE:-$(date +%F)}"
 allow_dirty=0
@@ -119,18 +275,31 @@ CURRENT_VERSION="$current_version" RELEASE_VERSION="$version" perl -0pi -e '
 ' README.md README_ZH.md
 
 info "Updating CHANGELOG.md"
+changelog_body=$(mktemp)
+generate_changelog_body >"$changelog_body"
+
 if grep -Fq "## [$version]" CHANGELOG.md; then
   RELEASE_VERSION="$version" RELEASE_DATE="$release_date" perl -0pi -e '
     s/^## \[\Q$ENV{RELEASE_VERSION}\E\] - [0-9]{4}-[0-9]{2}-[0-9]{2}$/## [$ENV{RELEASE_VERSION}] - $ENV{RELEASE_DATE}/m
   ' CHANGELOG.md
+
+  if changelog_section_has_todo; then
+    replace_changelog_body "$changelog_body"
+  fi
 else
   tmp=$(mktemp)
-  awk -v version="$version" -v release_date="$release_date" '
+  awk -v version="$version" -v release_date="$release_date" -v body_file="$changelog_body" '
+    function emit_body(  line) {
+      while ((getline line < body_file) > 0) {
+        print line
+      }
+      close(body_file)
+    }
+
     !inserted && /^## \[/ {
       print "## [" version "] - " release_date
       print ""
-      print "### Changed"
-      print "- TODO: Summarize release changes."
+      emit_body()
       print ""
       inserted = 1
     }
@@ -140,13 +309,14 @@ else
         print ""
         print "## [" version "] - " release_date
         print ""
-        print "### Changed"
-        print "- TODO: Summarize release changes."
+        emit_body()
       }
     }
   ' CHANGELOG.md >"$tmp"
   mv "$tmp" CHANGELOG.md
 fi
+
+rm -f "$changelog_body"
 
 if ! grep -Fq "[$version]:" CHANGELOG.md; then
   tmp=$(mktemp)
@@ -213,7 +383,11 @@ fi
 
 info "Release prep complete for v$version"
 printf '\nReview before tagging:\n'
-printf '  - Fill any TODO entries in CHANGELOG.md.\n'
+if grep -Fq 'TODO: Summarize release changes.' CHANGELOG.md; then
+  printf '  - Fill any TODO entries in CHANGELOG.md.\n'
+else
+  printf '  - Review the generated CHANGELOG.md entries.\n'
+fi
 printf '  - Review git diff.\n'
 printf '  - Commit with: chore(release): prepare v%s\n' "$version"
 printf '  - Tag with: git tag v%s && git push origin main v%s\n' "$version" "$version"
